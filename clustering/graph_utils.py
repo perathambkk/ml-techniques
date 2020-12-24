4807ee0c1b6166b17a7a070baf2d1ee5b47a1059
perathambkk <calzera.p@gmail.com>
2020-12-24 22:41:57 +0700

    add knn and gaussian similarity graphs

--- clustering/graph_utils.py
+++ clustering/graph_utils.py
@@ -20,6 +20,22 @@ def affinity_graph(X):
 			A[j][i] = dist # by symmetry
 	return A
 
+def knn_graph(X, knn=4):
+	'''
+	This function returns a numpy array.
+	'''
+	ni, nd = X.shape
+	nbrs = NearestNeighbors(n_neighbors=(knn+1), algorithm='ball_tree').fit(X)
+	distances, indices = nbrs.kneighbors(X)
+	A = np.zeros((ni, ni))
+	for ind in indices:
+		i0 = ind[0]
+		for i in ind[1:]:
+			dist = distances[ind[0],i]
+			A[ind[0], i] = dist
+			A[i, ind[0]] = dist # by symmetry
+	return A
+
 def sparse_affinity_graph(X):
 	'''
 	TODO: This function returns a numpy sparse matrix.
@@ -40,6 +56,12 @@ def laplacian_graph(X, mode='affinity', knn=3, eta=0.01, sigma=2.5):
 	if mode == 'affinity':
 		W = affinity_graph(X)
 		W[abs(W) > eta] = 0
+	elif mode == 'nearestneighbor':
+		W = knn_graph(X, knn=knn)
+	elif mode == 'gaussian':
+		W = affinity_graph(X)
+		bandwidth = 2.0*(sigma**2)
+		W = np.exp(W) / bangwidth
 	else:
 		pass
 	D = np.diag(W.sum(axis=1))
