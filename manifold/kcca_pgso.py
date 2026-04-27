5c00fbb42c062c8b7615915862f2622163425c87
Peratham <75246140+perathambkk@users.noreply.github.com>
2026-04-28 01:52:32 +0700

    add KCCA w/ PGSO

--- manifold/kcca_pgso.py
+++ manifold/kcca_pgso.py
@@ -12,84 +12,41 @@ from scipy import linalg as SLA
 from numpy import linalg as LA
 from sklearn.utils.extmath import row_norms, safe_sparse_dot
 
-# Function to check matrix 
-# is in lower triangular
-def islowertriangular(M):
-	for i in range(0, len(M)):
-		for j in range(i + 1, len(M)):
-			if(M[i][j] != 0): 
-					return False
-	return True
-
-def incomplete_cholesky_decomposition(K, nu = 1e-10):
+def partial_gram_schmidth_orthogonolization(Kin, nu=0.00001):
 	'''
-	Incomplete Cholesky Decomposition (ICD) described in Hardoon, D. R.;
-	Szedmak, S.; Shawe-Taylor, J. (2004)."Canonical Correlation Analysis:
+	Partial Gram-Schmidth Orthogonolization (PGSO) described in Hardoon, D. R.;
+	Szedmak, S.; Shawe-Taylor, J. (2004). "Canonical Correlation Analysis:
 	An Overview with Application to Learning Methods". Neural Computation.
 	16 (12): 2639–2664
 	
 
 	Inputs: K: an NxN matrix.
-			nu: a precision parameter, theoretically bounded 
-			    by an eigenvalue max_i Lambda_i of DeltaK_i.
+			nu: a precision parameter.
 	'''
-	# 1. Initinalization
-	N = K.shape[0]
-	i = 1
-	Kp = K.copy()
-	P = np.eye(N, N) # permutation
-	G = np.zeros([N, N]) # lower triangular with M non-zero elements
-	for j in range(N):
-		G[j, j] = K[j, j]
+	K = Kin.copy()
 
-	# 2. loop: python index starts from 0. range ends at the last indice - 1.
-	while np.sum(np.diag(G)) > nu and i != N + 1:
-		# find best new element
-		j_star = np.argmax(np.diag(G)) # indice?
-		j_star = (j_star + i) - 1
-		
-		# update permutation P
-		Pnext = np.eye(N, N)
-		Pnext[i, i] = 0
-		Pnext[j_star , j_star] = 0
-		Pnext[i, j_star] = 1
-		Pnext[j_star, i] = 1
-		P = P @ Pnext
+	# initialization
+	j = 0
+	m = K.shape[0]
+	size_vec = np.zeros(m)
+	index_vec = np.zeros(m)
+	feat = np.zeros([m, m])
+	norm2 = np.diag(K)
 
-		# Permute elements i and j_star in Kp
-		Kp = Pnext @ Kp @ Pnext
+	# algorithm
+	while np.sum(norm2) > nu and j != N:
+		ij = np.argmax(norm2)
+		index_vec[j] = ij
+		size_vec[j] = np.sqrt(norm2[ij])
+		for i in range(m): # not vectorized
+			sum_ff = 0.0
+			for t in range(j):
+				sum_ff += feat[i, t] * feat[ij, t]
+			feat[i, j] = (K[i, ij] - sum_ff)/size_vec[j] # ?
+			norm2[i] = norm2[i] - feat[i, j] ** 2
+		j += 1
 
-		# update G (I have never been thinking about people trying 
-		#          to kill other via gassing attacks because of 
-		#          implementing this algorithm.... -.-)
-		for k in range(i - 1): # 1:i-1 not vectorized
-			G[i, k], G[j_star, k] = G[j_star, k], G[i, k]
-
-		# permute elements j_star, j_star and i, i of G
-		G[i, i], G[j_star, j_star] = G[j_star, j_star], G[i, i]
-		G[i, i] = np.sqrt(G[i, i])
-		print(G[i, i])
-
-		# calculate i^{th} column of G
-		for k in range(i, N): # i+1:N not vectorized
-			GG = 0
-			for j in range(i - 1): # j 1:i-1 not vectorized
-				GG += G[k, j] * G[i, j]
-			G[k, i] = (Kp[k, i] - GG) / G[i, i]
-			
-
-		# Update only diagonal elements
-		for j in range(i, N): # j i+1:N not vectorized
-			sum_Gjk_sq = 0
-			for k in range(i): # 1:i not vectorized
-				sum_Gjk_sq += G[j, k] ** 2
-			G[j, j] = Kp[j, j] - sum_Gjk_sq
-
-		# update i
-		i = i +1
-		
-	M = i
-	return P, G, M
+	return feat
 
 def affinity_graph(X):
 	'''
@@ -168,7 +125,7 @@ def _sq_inverse(Xin):
 	Xres = V.conj() @ L @ V.T.conj()
 	return Xres
 
-def kcca_chol(Xin, Yin, reg_kappa=0.01):
+def kcca_reg_chol(Xin, Yin, reg_kappa=0.01):
 	"""
 	Perform Regularized Kernel Canonical Correlation Analysis (RKCCA) on an input row matrix X ,Y.
 
@@ -202,24 +159,9 @@ def kcca_chol(Xin, Yin, reg_kappa=0.01):
 	KY = _apply_kernel(Y, kernel=kernel_type, params=kernel_params)
 
 	# cross correlation
-	# Cxy = X.T.conj() @ Y
-	# Cxx = X.T.conj() @ X
-	# # Cyx = Y.T.conj() @ X
-	# Cyy = Y.T.conj() @ Y
-
-	# sqrt inverse
-	# Cxx_sqinv = _sq_inverse(Cxx)
-	# Cyy_sqinv = _sq_inverse(Cyy)
-	# B = Cxx_sqinv @ Cxy @ Cyy_sqinv
-
-	# eigen decomposition
-	# I_mat = np.eyes(KX.shape[0], KX.shape[1])
-	# shrink_KX = KX + (reg_kappa * I_mat)
-	# I_mat = np.eyes(KY.shape[0], KY.shape[1])
-	# shrink_KY = KY + (reg_kappa * I_mat)
-	_, S ,_ = incomplete_cholesky_decomposition(KX)
-	_, Rx ,_ = incomplete_cholesky_decomposition(KX)
-	_, Ry ,_ = incomplete_cholesky_decomposition(KY)
+	_, S ,_ = partial_gram_schmidth_orthogonolization(KX)
+	_, Rx ,_ = partial_gram_schmidth_orthogonolization(KX)
+	_, Ry ,_ = partial_gram_schmidth_orthogonolization(KY)
 
 	Zxx = Rx.T @ Rx 
 	Zyy = Ry.T @ Ry
@@ -233,7 +175,7 @@ def kcca_chol(Xin, Yin, reg_kappa=0.01):
 	u = u[:, ind]
 
 	# B = LA.pinv(shrink_KY) @ KX @ LA.pinv(shrink_KX) @ KY
-	_, S ,_ = incomplete_cholesky_decomposition(KY)
+	_, S ,_ = partial_gram_schmidth_orthogonolization(KY)
 	B = LA.pinv(S) @ Zyx @ LA.pinv(Zxx) @ Zxy @ LA.pinv(S.T)
 	sv, v = LA.eigh(B)
 	ind = np.argsort(-sv, axis=0) # sorting descending
